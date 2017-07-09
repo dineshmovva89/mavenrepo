0ad0237759460029de17f1ca92115566c667ad9e
dineshmovva89 <movvdinesh89@gmail.com>
2017-07-09 18:35:00 -0400

    Initial Commit

---
+++ src/main/java/com/mkyong/common/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.mkyong.common;
+
+/**
+ * Hello sarath this is your first project :)
+ *
+ */
+public class HelloWorld 
+{
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+}
